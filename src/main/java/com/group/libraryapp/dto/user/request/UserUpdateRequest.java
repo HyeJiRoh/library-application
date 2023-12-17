d76370935189a956080173fbddd40e69ed6a118c
HyeJiRoh <shgpwl509@gmail.com>
2023-12-18 02:15:55 +0900

    first commit

---
+++ src/main/java/com/group/libraryapp/dto/user/request/UserUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.group.libraryapp.dto.user.request;
+
+public class UserUpdateRequest {
+    private long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
