af1cc692b17e34ededa5dd18c5b254c32e9fb027
HyeJiRoh <shgpwl509@gmail.com>
2023-12-18 16:37:28 +0900

    add lombok

--- src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
+++ src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
@@ -1,14 +1,9 @@
 package com.group.libraryapp.dto.user.request;
 
+import lombok.Getter;
+
+@Getter
 public class UserCreateRequest {
     private String name;
     private Integer age; //int는 null을 표현할 수 없고, Integer는 null을 표현할 수 있다.
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
 }
